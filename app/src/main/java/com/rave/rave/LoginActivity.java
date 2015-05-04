a77f47f12f0416f6d05d0cb11b3a43d86285a2be
kshallow <keegans@live.com>
2015-05-04 16:28:36 -0500

    login working, no integration with Jacobs views

--- app/src/main/java/com/rave/rave/LoginActivity.java
+++ app/src/main/java/com/rave/rave/LoginActivity.java
@@ -1,92 +1,165 @@
 package com.rave.rave;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.List;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import Model.Backend;
-import Model.User;
+import java.io.IOException;
+
+import savage.UrlJsonAsyncTask;
 
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends Activity {
-    private static final String TAG = "LoginActivity";
+public class LoginActivity extends ActionBarActivity {
+
+    private final static String LOGIN_API_ENDPOINT_URL = "http://madrave.herokuapp.com/api/v1/sessions";
+    private SharedPreferences mPreferences;
+    private String mUserEmail;
+    private String mUserPassword;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
     }
 
-    public void login(View view){
-        EditText emailView = (EditText)findViewById(R.id.username);
-        EditText pwView = (EditText) findViewById(R.id.password);
-        login(String.valueOf(emailView.getText()), String.valueOf(pwView.getText()));
+    public void login(View button) {
+        EditText userEmailField = (EditText) findViewById(R.id.userEmail);
+        mUserEmail = userEmailField.getText().toString();
+        EditText userPasswordField = (EditText) findViewById(R.id.userPassword);
+        mUserPassword = userPasswordField.getText().toString();
+
+        if (mUserEmail.length() == 0 || mUserPassword.length() == 0) {
+            // input fields are empty
+            Toast.makeText(this, "Please complete all the fields",
+                    Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            LoginTask loginTask = new LoginTask(LoginActivity.this);
+            loginTask.setMessageLoading("Logging in...");
+            loginTask.execute(LOGIN_API_ENDPOINT_URL);
+        }
     }
 
-    /* API Methods */
-    private void login(String email, String password) {
-        final Context currContext = this;
-        Log.d(TAG, "Attempting to login with email: " + email + " password: " + password);
-        Backend.logIn(email, password, new Backend.BackendCallback() {
-            @Override
-            public void onRequestCompleted(Object result) {
-                final User user = (User) result;
-                Log.d(TAG, "Login success. User: " + user.toString());
 
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        //Must check db for user with existing backendId.  If doesn't already exist, then save
-                        //.find is how to query for objects saved with Sugar
-                        List<User> users = User.find(User.class, "backend_id = ?", new Integer(user.backendId).toString());
-                        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(currContext);
-                        SharedPreferences.Editor editor = prefs.edit();
-                        if (users.size() == 0) {
-                            user.save();
-                            //This is NOT the backend id, this is the sugar id
-                            editor.putString("loggedInId", Long.toString(user.getId()));
-                            editor.commit();
-                        } else {
-                            User currUser = users.get(0);
-                            currUser.authToken = user.authToken;
-                            currUser.tokenExpiration = user.tokenExpiration;
-                            currUser.save();
-                            //This is NOT the backend id, this is the sugar id
-                            editor.putString("loggedInId", Long.toString(currUser.getId()));
-                            editor.commit();
-                        }
+    private class LoginTask extends UrlJsonAsyncTask {
+        public LoginTask(Context context) {
+            super(context);
+        }
 
-                        Intent intent = new Intent(currContext, MainActivity.class);
-                        startActivity(intent);
-                    }
-                });
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);   //urls[0] api/v1/sessions
+            JSONObject holder = new JSONObject();
+            JSONObject userObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
+
+            try {
+                try {
+                    // setup the returned values in case
+                    // something goes wrong
+//                    json.put("success", false);
+//                    json.put("info", "Something went wrong. Retry!");
+                    // add the user email and password to
+                    // the params
+                    userObj.put("email", mUserEmail);
+                    userObj.put("password", mUserPassword);
+                    holder.put("user", userObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
+
+                    // setup the request headers
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                    json.put("info", "Email and/or password are invalid. Retry!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
             }
 
-            @Override
-            public void onRequestFailed(final String message) {
+            return json;
+        }
 
-                //NOTE: parameter validation and filtering is handled by the backend, just show the
-                //returned error message to the user
-                Log.d(TAG, "Received error from Backend: " + message);
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-                    }
-                });
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if (json.getBoolean("success")) {
+                    // everything is ok
+                    SharedPreferences.Editor editor = mPreferences.edit();
+                    // save the returned auth_token into
+                    // the SharedPreferences
+                    editor.putString("AuthToken", json.getJSONObject("data").getString("auth_token"));
+                    editor.commit();
+
+
+                    String token = mPreferences.getString("AuthToken", "missing");
+                    Log.d("bimbam", token);
+                    // launch the HomeActivity and close this one
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                Toast.makeText(context, json.getString("info"), Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                // something went wrong: show a Toast
+                // with the exception message
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                super.onPostExecute(json);
             }
-        });
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
-
-
---
+++ app/src/main/java/com/rave/rave/RegisterActivity.java
@@ -0,0 +1,82 @@
+package com.rave.rave;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class RegisterActivity extends ActionBarActivity {
+
+    private final static String REGISTER_API_ENDPOINT_URL = "http://10.0.2.2:3000/api/v1/registrations";
+    private SharedPreferences mPreferences;
+    private String mUserEmail;
+    private String mUserName;
+    private String mUserPassword;
+    private String mUserPasswordConfirmation;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+    }
+
+    public void registerNewAccount(View button) {
+        EditText userEmailField = (EditText) findViewById(R.id.userEmail);
+        mUserEmail = userEmailField.getText().toString();
+        EditText userNameField = (EditText) findViewById(R.id.userName);
+        mUserName = userNameField.getText().toString();
+        EditText userPasswordField = (EditText) findViewById(R.id.userPassword);
+        mUserPassword = userPasswordField.getText().toString();
+        EditText userPasswordConfirmationField = (EditText) findViewById(R.id.userPasswordConfirmation);
+        mUserPasswordConfirmation = userPasswordConfirmationField.getText().toString();
+
+        if (mUserEmail.length() == 0 || mUserName.length() == 0 || mUserPassword.length() == 0 || mUserPasswordConfirmation.length() == 0) {
+            // input fields are empty
+            Toast.makeText(this, "Please complete all the fields",
+                    Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            if (!mUserPassword.equals(mUserPasswordConfirmation)) {
+                // password doesn't match confirmation
+                Toast.makeText(this, "Your password doesn't match confirmation, check again",
+                        Toast.LENGTH_LONG).show();
+                return;
+            } else {
+//                // everything is ok!
+//                RegisterTask registerTask = new RegisterTask(RegisterActivity.this);
+//                registerTask.setMessageLoading("Registering new account...");
+//                registerTask.execute(REGISTER_API_ENDPOINT_URL);
+           }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
